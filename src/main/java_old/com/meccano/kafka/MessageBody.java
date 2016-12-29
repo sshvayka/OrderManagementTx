e445258301396b4d4a2e550937e3055004e72442
rubencasado <ruben@lambdoop.com>
2016-12-29 11:46:23 +0100

    deleted old java folder

--- src/main/java_old/com/meccano/kafka/MessageBody.java
+++
@@ -1,9 +0,0 @@
-package com.meccano.kafka;
-
-/**
- * Created by ruben.casado.tejedor on 30/08/2016.
- *
- * Single interface used to all Request/Response message
- */
-public interface MessageBody {
-}
